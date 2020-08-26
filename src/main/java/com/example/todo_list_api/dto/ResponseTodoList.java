09c2ccd67633c87f2b44575f51bd5ca0730df7e1
你的用户名 <997915421@qq.com>
2020-08-26 18:49:57 +0800

    [TodoList_Api][Fennie] feature:should_retun_size_2_when_todolist_have_2_item

--- src/main/java/com/example/todo_list_api/dto/ResponseTodoList.java
+++ src/main/java/com/example/todo_list_api/dto/ResponseTodoList.java
@@ -1,5 +1,7 @@
 package com.example.todo_list_api.dto;
 
+import com.example.todo_list_api.entity.TodoListItem;
+
 public class ResponseTodoList {
     private String id;
     private String content;
@@ -28,4 +30,13 @@ public class ResponseTodoList {
     public void setStatus(boolean status) {
         this.status = status;
     }
+
+    public static ResponseTodoList mapResponseTodoToTodoItem(TodoListItem todoListItem) {
+        ResponseTodoList responseTodo = new ResponseTodoList();
+        responseTodo.setId(String.valueOf(todoListItem.getId()));
+        responseTodo.setContent(todoListItem.getContent());
+        responseTodo.setStatus(todoListItem.getStatus().equals("true"));
+        return responseTodo;
+    }
+
 }
--- src/main/java/com/example/todo_list_api/service/TodoListServiceImpl.java
+++ src/main/java/com/example/todo_list_api/service/TodoListServiceImpl.java
@@ -2,14 +2,22 @@ package com.example.todo_list_api.service;
 
 import com.example.todo_list_api.dto.ResponseTodoList;
 import com.example.todo_list_api.entity.TodoListItem;
+import com.example.todo_list_api.respository.TodoListRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TodoListServiceImpl implements TodoListService{
 
+    @Autowired
+    private TodoListRespository todoListRespository;
+
     @Override
     public List<ResponseTodoList> getTodoList() {
-        return null;
+        List<TodoListItem> todoListItemList = todoListRespository.findAll();
+        List<ResponseTodoList> responseTodoListList = new ArrayList<>();
+        todoListItemList.forEach(todoListItem -> responseTodoListList.add(ResponseTodoList.mapResponseTodoToTodoItem(todoListItem)));
+        return responseTodoListList;
     }
 }
